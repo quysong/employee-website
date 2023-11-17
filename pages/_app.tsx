0d780fd62d2774cf29b712045b225104b9ee64eb
anh.ly <anh.ly@credify.one>
2023-11-17 21:26:56 +0700

    comment CSP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
 import "assets/globalStyles.css";
+import NavigationBottomContainer from "containers/navigation-bottom/navigation-bottom";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "styled-components";
 import { useLocalStorage } from "usehooks-ts";
 import { defaultTheme } from "../components/themes/defaultTheme";
 import { GlobalStyle } from "../components/themes/globalStyle";
-import NavigationBottomContainer from "containers/navigation-bottom/navigation-bottom";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [theme] = useLocalStorage("theme", defaultTheme);
-  const {pathname} = useRouter();
+  const { pathname } = useRouter();
   return (
     <>
       <Head>
@@ -18,12 +18,12 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
         ></meta>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+        {/* <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta> */}
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
-        {!pathname.includes('onboard') &&<NavigationBottomContainer/>}
+        {!pathname.includes("onboard") && <NavigationBottomContainer />}
       </ThemeProvider>
     </>
   );
